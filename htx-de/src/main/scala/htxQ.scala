e0929f0d9a11c99d6bed6db6a2ef210f287159ac
alex chen <alex_chen@cpf.gov.sg>
2025-05-31 22:27:11 +0800

    commit

--- htx-de/src/main/scala/htxQ.scala
+++ htx-de/src/main/scala/htxQ.scala
@@ -1,9 +1,38 @@
 import org.apache.spark.sql.{DataFrame, SparkSession}
+import org.apache.spark.sql.functions.{col, countDistinct}
 
 class htxQ_class(spark: SparkSession) {
-  def read_parquet_df(path: String): Unit = {
-    val df: DataFrame = spark.read.parquet(path)
-    df.show()
+  def read_parquet_df(path: String): DataFrame = {
+    spark.read.parquet(path)
+  }
+
+  def compute_item_rank(trans: DataFrame, ref_table: DataFrame): DataFrame = {
+    val joined_df = trans.alias("trans")
+      .join(
+        ref_table.alias("ref_table"),
+        col("trans.geographical_location_oid") === col("ref_table.geographical_location_oid"),
+        "left"
+      )
+
+    val item_rank_df = joined_df
+      .groupBy(
+        col("trans.geographical_location_oid"),
+        col("item_name")
+      )
+      .agg(
+        countDistinct("detection_oid").alias("item_rank")
+      )
+      .orderBy(
+        col("trans.geographical_location_oid"),
+        col("item_rank").desc
+      )
+      .select(
+        col("trans.geographical_location_oid"),
+        col("item_rank").cast("string"),
+        col("item_name")
+      )
+
+    item_rank_df
   }
 }
 
@@ -11,10 +40,15 @@ object Main {
   def main(args: Array[String]): Unit = {
     val spark = SparkSession.builder
       .appName("htx-de")
-      .master("local[*]") 
+      .master("local[*]")
       .getOrCreate()
-    val printer = new htxQ_class(spark)
-    printer.read_parquet_df("/Users/alexchen/Documents/repo/htx/htx_q/data/trans.parquet")
+    val htxC = new htxQ_class(spark)
+    val trans = htxC.read_parquet_df("/Users/alexchen/Documents/repo/htx/htx_q/data/trans.parquet")
+    val ref_table = htxC.read_parquet_df("/Users/alexchen/Documents/repo/htx/htx_q/data/ref_table.parquet")
+
+    val item_rank_df = htxC.compute_item_rank(trans, ref_table)
+    item_rank_df.show(100, truncate = false)
+
     spark.stop()
   }
 }
